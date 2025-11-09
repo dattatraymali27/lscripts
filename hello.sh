02fade6e4a4358b2291b48af263feb96c8c73785
dattatraymali27 <dattatraymali9@gmail.com>
2025-11-09 09:51:03 +0530

    Add files via upload

---
+++ hello.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#this is comment
+echo good morning
+
+echo our shell name is $BASH
+echo our shell version name is $BASH_VERSION
+echo hour home directory name is $HOME
+echo our current working directory is $PWD
+
+name=mark
+val=10
+echo the name is $name
+echo value is $val
+
+
